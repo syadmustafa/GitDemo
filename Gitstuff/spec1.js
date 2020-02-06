f6482659c88b77483ba89ded78a545801bcbbd95
Al Ahdal <syed.ahdal@philips.com>
2020-02-06 16:21:40 -0500

    first commit

---
+++ Gitstuff/spec1.js
@@ -0,0 +1,34 @@
+describe('Protractor baby steps',function() {  
+
+
+    it('Open Angular js website',function() {
+        browser.get('https:/angularjs.org');
+        
+    
+    
+    
+    
+    
+    //write your protractor raw code
+    
+    
+    
+    })
+    
+     
+    
+    
+    //Each it block will be called as a spec
+    
+    
+    
+    it('close browswer', function() {
+    
+    
+    
+    // code to close browser
+    })
+    
+    
+    })
+    
